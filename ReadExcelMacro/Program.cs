edc6422b81b8f0b9e390ba9f478b33bc110932bd
Leonardo Queres <leonardoqueres@hotmail.com>
2019-09-05 15:13:09 -0300

    Implementação de um metodo para execução da leitura do arquivo.

--- ReadExcelMacro/Program.cs
+++ ReadExcelMacro/Program.cs
@@ -8,48 +8,52 @@ namespace ReadExcelMacro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Leitura de arquivo EXCEL(XLSM).");
 
             string fileName = @"C:\Users\leonardo.queres\source\repos\ReadExcelMacro\ReadExcelMacro\Excel\excel.xlsm";
 
-            var wb = new XLWorkbook(fileName);
-            var ws = wb.Worksheet(1).RangeUsed();
+            LerExcel(fileName);
+        }
 
-            // Look for the first row used
-            var firstRowUsed = ws.FirstRowUsed();
-
-            // Narrow down the row so that it only includes the used part
-            var row = firstRowUsed.RowUsed();
-
-            // Move to the next row (it now has the titles)
-            //categoryRow = categoryRow.RowBelow();
-
-            var cont = 1;
-            var totalLinha = 1;
-            while (!row.IsEmpty())
+        private static void LerExcel(string fileName)
+        {
+            using (var wb = new XLWorkbook(fileName))
             {
-                Console.WriteLine(totalLinha++);
-                while (cont <= 23)
+                var ws = wb.Worksheet(1).RangeUsed();
+
+                // Look for the first row used
+                var firstRowUsed = ws.FirstRowUsed();
+
+                // Narrow down the row so that it only includes the used part
+                var row = firstRowUsed.RowUsed();
+
+                // Move to the next row (it now has the titles)
+                //categoryRow = categoryRow.RowBelow();
+
+                var cont = 1;
+                var totalLinha = 1;
+                while (!row.IsEmpty())
                 {
-                    var categoryName = row.Cell(cont).GetString();
-                    
-                    Console.WriteLine(cont + " - " + categoryName);
-                    cont++;
+                    Console.WriteLine(totalLinha++);
+                    while (cont <= 23)
+                    {
+                        var categoryName = row.Cell(cont).GetString();
+
+                        Console.WriteLine(cont + " - " + categoryName);
+                        cont++;
+                    }
+
+                    row = row.RowBelow();
+                    cont = 1;
+
+
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    Console.WriteLine("--------------------------------------------------------------------");
                 }
-
-                row = row.RowBelow();
-                cont = 1;
-
-
-                Console.WriteLine("--------------------------------------------------------------------");
-                Console.WriteLine("--------------------------------------------------------------------");
-                Console.WriteLine("--------------------------------------------------------------------");
-                Console.WriteLine("--------------------------------------------------------------------");
+                Console.ReadLine();
             }
-
-            
-            
-            Console.ReadLine();
         }
     }
 }

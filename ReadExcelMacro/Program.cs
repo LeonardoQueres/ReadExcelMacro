ecfffaea831e138bfbed01052342b50d7f87c925
Leonardo Queres <leonardoqueres@hotmail.com>
2019-09-05 12:41:13 -0300

    Projeto

---
+++ ReadExcelMacro/Program.cs
@@ -0,0 +1,55 @@
+﻿using ClosedXML.Excel;
+using System;
+
+
+namespace ReadExcelMacro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string fileName = @"C:\Users\leonardo.queres\source\repos\ReadExcelMacro\ReadExcelMacro\Excel\excel.xlsm";
+
+            var wb = new XLWorkbook(fileName);
+            var ws = wb.Worksheet(1).RangeUsed();
+
+            // Look for the first row used
+            var firstRowUsed = ws.FirstRowUsed();
+
+            // Narrow down the row so that it only includes the used part
+            var row = firstRowUsed.RowUsed();
+
+            // Move to the next row (it now has the titles)
+            //categoryRow = categoryRow.RowBelow();
+
+            var cont = 1;
+            var totalLinha = 1;
+            while (!row.IsEmpty())
+            {
+                Console.WriteLine(totalLinha++);
+                while (cont <= 23)
+                {
+                    var categoryName = row.Cell(cont).GetString();
+                    
+                    Console.WriteLine(cont + " - " + categoryName);
+                    cont++;
+                }
+
+                row = row.RowBelow();
+                cont = 1;
+
+
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("--------------------------------------------------------------------");
+            }
+
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
